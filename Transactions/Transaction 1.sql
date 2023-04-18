9ae055610f49a3360fa9859a41a30b6a6bd66fed
shreyas21563 <108022785+shreyas21563@users.noreply.github.com>
2023-04-18 09:12:41 +0530

    Update and rename Transaction 1.sql to Transaction .sql

--- Transactions/Transaction 1.sql
+++
@@ -1,8 +0,0 @@
-USE dailydairy;
-START TRANSACTION;
-SET @order_id = (SELECT MAX(order_id) FROM `order`) + 1;
-SET @order_value = (SELECT cart_value FROM Cart WHERE cart.cart_id=1);
-INSERT INTO `order` VALUES (@order_id, 'Order Confirmed', 1, 1, 1, @order_value);
-INSERT INTO `order_placing`(`product_id`, `order_id`, `cart_id`, `quantity`) SELECT `product_id`, @order_id, 1, quantity FROM Adds where Customer_ID=1;
-DELETE FROM adds where customer_id=1;
-COMMIT;		

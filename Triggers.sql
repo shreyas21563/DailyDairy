387e5224274952ba2f4128c4fa693919aae580f5
shreyas21563 <108022785+shreyas21563@users.noreply.github.com>
2023-07-19 20:01:26 +0530

    Update Triggers.sql

--- Triggers.sql
+++ Triggers.sql
@@ -16,12 +16,3 @@ FOR EACH ROW
     SET Cart_Value = Cart_Value - Product.Product_Value * OLD.Quantity
     WHERE Cart_ID = OLD.Cart_ID AND Product.Product_ID = OLD.Product_ID;
 
--- CREATE TRIGGER `update_avg_rating` AFTER INSERT ON `gives_product_feedback`
--- FOR EACH ROW 
--- 	UPDATE `Product`
---     SET Average_Rating = (
---         SELECT AVG(gives_product_feedback.Rating) 
---         FROM gives_product_feedback 
---         WHERE gives_product_feedback.Product_ID = NEW.Product_ID
---     ) 
---     WHERE `Product_ID` = NEW.Product_ID;
